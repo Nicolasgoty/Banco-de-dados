d9604a796fc455b8f64d518078adab835f428b3d
nicolas <nicolas.anastacio.pereira@escola.pr.gov.br>
2026-03-27 07:46:31 -0300

    Add sample data to Aluno, Professor, and Escola
    Inserted sample data into Aluno, Professor, and Escola tables.

---
+++ cadastroescolar.sql
@@ -0,0 +1,21 @@
+-- Inserindo dados na tabela Aluno
+INSERT INTO Aluno (id_aluno, nome, idade)
+VALUES
+(1, 'Ana Costa', 18),
+(2, 'João Silva', 20),
+(3, 'Maria Oliveira', 19);
+(4, 'Maria João', 21);
+(5, 'Maria Carla', 22);
+
+INSERT INTO Professor (id_professor, nome, cpf)
+VALUES
+(1, 'Ricardo Arona', '11122233344'),
+(2, 'Fernanda Montenegro', '55566677788'),
+(3, 'Sérgio Moro', '99900011122');
+
+-- Inserindo dados na tabela Escola
+INSERT INTO Escola (id_escola, nome_escola, cidade)
+VALUES
+(1, 'Escola Machado de Assis', 'Rio de Janeiro'),
+(2, 'Colégio Dom Pedro II', 'São Paulo'),
+(3, 'Instituto de Educação', 'Belo Horizonte');
